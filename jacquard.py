ebf66e8be7311c324b430ee6721a99fa08d0765c
Gokul Das B <gokuldas.b@gmail.com>
2012-12-07 18:32:49 +0000

    Initial commit with working algorithm.

---
+++ jacquard.py
@@ -0,0 +1,22 @@
+def bigram_shingler(str):
+    """Extract a set of 2 character n-grams (character bigrams) from string"""
+    big_set = {str[x:x+2] for x in range(0, len(str) -1) if len(str) > 1}
+    return big_set
+
+def jacq_ind(bigrams1, bigrams2):
+    """Calculate Jacquard index for 2 bigrams"""
+    unin = bigrams1 | bigrams2
+    intn = bigrams1 & bigrams2
+    if len(unin) == 0: return 0.0
+    else: return len(intn) / len(unin)
+
+from corpus import text
+words = text.split()
+corpus = {i : bigram_shingler(i) for i in words}
+
+str = input("Enter a word. ").strip()
+in_big = bigram_shingler(str)
+index_list = [(i, jacq_ind(in_big, corpus[i])) for i in corpus]
+sorted_list = sorted(index_list, key = lambda x : x[1], reverse = True)
+print("\nThe 3 best matches are:")
+for i in range(3): print(sorted_list[i][0])
